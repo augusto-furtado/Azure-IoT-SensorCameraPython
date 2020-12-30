258f9677ce4dd7aeab844531921686d7f1a0c3c1
augusto.furtado <augusto.furtado@accenture.com>
2020-12-30 13:01:30 -0300

    finishing app

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 SensorCamera
-This app take a picture using a camera (OmniVision OV5647 - 5MP) attached to RaspberryPI and send notification to IoT Hub.
-
+This app take a picture using a camera (OmniVision OV5647 - 5MP) attached to Raspberry PI and send notification to IoT Hub.
+The trigger to take a picture is through a direct method call named "TakePicture"
 
 Requirements:
 -If connecting to an Azure IoT Edge Gateway, certificate must be installed on Host OS, for linux use:
@@ -10,7 +10,7 @@ sudo update-ca-certificates
 -pip3 install picamera
 
 
-
 References:
+https://github.com/Azure/azure-iot-sdk-python
 https://docs.microsoft.com/pt-br/azure/iot-hub/quickstart-send-telemetry-python
 https://docs.microsoft.com/pt-br/azure/iot-edge/how-to-connect-downstream-device?view=iotedge-2018-06
--- sensorcameraapp.py
+++ sensorcameraapp.py
@@ -6,43 +6,25 @@ import time
 import threading
 import datetime
 from picamera import PiCamera
-
-# Using the Python Device SDK for IoT Hub:
-#   https://github.com/Azure/azure-iot-sdk-python
-# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
 # The device connection string to authenticate the device with your IoT hub.
-# Using the Azure CLI:
-# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
 CONNECTION_STRING = "HostName=iothubbyaugusto.azure-devices.net;DeviceId=device1;SharedAccessKey=bGyzWD5D04IOJDSiRK13tYHizgrMqBFa+MfwEB+eauo=;GatewayHostName=raspberrypi"
-
-# Define the JSON message to send to IoT Hub.
-TEMPERATURE = 20.0
-HUMIDITY = 60
-MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
-
-INTERVAL = 5
 
 def iothub_client_init():
     # Create an IoT Hub client
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
     return client
 
-
 def device_method_listener(device_client):
-    global INTERVAL
     while True:
         method_request = device_client.receive_method_request()
-        print (
-            "\nMethod callback called with:\nmethodName = {method_name}\npayload = {payload}".format(
-                method_name=method_request.name,
-                payload=method_request.payload
-            )
-        )
+        
         if method_request.name == "TakePicture":
             try:
+                #Example how to read the payload content
                 #payload = method_request.payload
+                
                 take_picture()
                 send_telemetry(device_client)
             except ValueError:
@@ -70,34 +52,32 @@ def main():
 
         while True:
             time.sleep(10)
-            print("alive...")
+            print(".")
     except KeyboardInterrupt:
-        print ( "IoTHubClient sample stopped" )
+        print ( "App stopped" )
 
 def send_telemetry(device_client):
 
     try:        
-        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
-        
         # Build the message with simulated telemetry values.
-        temperature = TEMPERATURE + (random.random() * 15)
-        humidity = HUMIDITY + (random.random() * 20)
-        msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
-        message = Message(msg_txt_formatted)
+        
+        # Example to send payload using json format
+        #MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
+        #temperature = TEMPERATURE + (random.random() * 15)
+        #humidity = HUMIDITY + (random.random() * 20)
+        #msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+        #message = Message(msg_txt_formatted)
+
+        #Send payload as a simple text
+        message = Message("newImage")
 
         # Add a custom application property to the message.
-        # An IoT hub can filter on these properties without access to the message body.
-        if temperature > 30:
-          message.custom_properties["temperatureAlert"] = "true"
-        else:
-          message.custom_properties["temperatureAlert"] = "false"
-
+        message.custom_properties["source"] = "edgeRaspberryPI"
+        
         # Send the message.
-        print( "Sending message: {}".format(message) )
         device_client.send_message(message)
         print( "Message sent" )
-        time.sleep(INTERVAL)
-
+        
     except Exception as ex:
         print("Exception to send telemetry: %s" % ex)
 
